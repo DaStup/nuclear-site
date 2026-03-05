3f50d5577c7707c3fb83a88b08041bad45b91808
danjel.stupar@student.htl-rankweil.at <danjel.stupar@student.htl-rankweil.at>
2026-03-05 11:28:57 +0000

    Edited SiteOverview

--- client_code/SiteOverview/__init__.py
+++ client_code/SiteOverview/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import SiteOverviewTemplate
 from anvil import *
+import plotly.graph_objects as go
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -9,12 +10,11 @@ from anvil.tables import app_tables
 
 
 class SiteOverview(SiteOverviewTemplate):
+  reactor_data = [{0}, {0}]
   def __init__(self, **properties):
     self.init_components(**properties)
     site_name = properties['my_parameter']
     self.headline_nuclear_site.text = site_name
-    self.canvas_reactor_data.height = 300
-    self.canvas_reactor_data.reset_context()
     site_id = anvil.server.call("query_facility_id", f"{site_name}")[0]['FacilityID']
     reactors_ids = [int(item[0]) for item in anvil.server.call("query_reactors", f"{site_id}")]
     reactor_names = []
@@ -22,17 +22,22 @@ class SiteOverview(SiteOverviewTemplate):
       reactor_names.append("Reactor " + str(id));
     self.drop_down_reactor.items = list(zip(reactor_names, reactors_ids))
     self.reactor_data = anvil.server.call("query_reactor_data", f"{self.drop_down_reactor.selected_value}")
+    self.plot_reactor.data = [
+      go.Bar(
+        x = [0],
+        y = [self.reactor_data[0]["Pressure"]],
+        name = 'Pressure'
+      )
+    ]
+   
   
-
-  @handle("canvas_reactor_data", "reset")
-  def canvas_reactor_data_reset(self, **event_args):
-    rdc = self.canvas_reactor_data
-    rdc.scale(2, 2)
-    rdc.stroke_style = "#2196F3"
-    rdc.line_width = 3
-    rdc.fill_style = "#FF0000"
-    rdc.fill_rect(10, 10, 10, self.reactor_data["Pressure"])
-
   @handle("drop_down_reactor", "change")
   def drop_down_reactor_change(self, **event_args):
     self.reactor_data = anvil.server.call("query_reactor_data", f"{self.drop_down_reactor.selected_value}")
+    self.plot_reactor.data = [
+      go.Bar(
+        x = [0],
+        y = [self.reactor_data[0]["Pressure"]],
+        name = 'Pressure'
+      )
+    ]
