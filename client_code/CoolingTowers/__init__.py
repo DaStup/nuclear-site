5e2b9e92845007dfbfd687232e74789f3adc2fab
danjel.stupar@student.htl-rankweil.at <danjel.stupar@student.htl-rankweil.at>
2026-03-18 09:29:00 +0000

    Created Nuclear-power-plant.svg; Edited CoolingTowers, SiteOverview and Backend

--- client_code/CoolingTowers/__init__.py
+++ client_code/CoolingTowers/__init__.py
@@ -7,16 +7,56 @@ from anvil.tables import app_tables
 
 
 class CoolingTowers(CoolingTowersTemplate):
+  
+    
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.site_id = properties["site_id"]
+    self.reactor_id = properties["reactor_id"]
+    self.tower_ids = [int(item[0]) for item in anvil.server.call("query_towers", f"{self.reactor_id}")]
+    self.tower_names = []
+    for id in self.tower_ids:
+      self.tower_names.append("Tower" + str(id))
+    self.drop_down_cooling_tower.items = list(zip(self.tower_names, self.tower_ids))
+    self.tower_data = anvil.server.call("query_tower_data", self.drop_down_cooling_tower.selected_value)
+    self.update_tower_data()
 
+  
   @handle("button_back", "click")
   def button_back_click(self, **event_args):
     open_form('SiteOverview', site_id=f"{self.site_id}")
 
   @handle("drop_down_cooling_tower", "change")
   def drop_down_cooling_tower_change(self, **event_args):
-    """This method is called when an item is selected"""
-    pass
+    self.update_tower_data()
+
+  @handle("button_toggle_sys1", "click")
+  def button_toggle_sys1_click(self, **event_args):
+    if(self.tower_data[0]["StatusPrim"] == 0):
+      anvil.server.call("update_prim_status", self.drop_down_cooling_tower.selected_value, 1)
+    else:
+       anvil.server.call("update_prim_status", self.drop_down_cooling_tower.selected_value, 0)
+    self.update_tower_data()
+
+    
+  @handle("button_toggle_sys2", "click")
+  def button_toggle_sys2_click(self, **event_args):
+    if(self.tower_data[0]["StatusSec"] == 0):
+      anvil.server.call("update_sec_status", self.drop_down_cooling_tower.selected_value, 1)
+    else:
+      anvil.server.call("update_sec_status", self.drop_down_cooling_tower.selected_value, 0)
+    self.update_tower_data()
+
+  def update_tower_data(self):
+    self.tower_data = anvil.server.call("query_tower_data", self.drop_down_cooling_tower.selected_value)
+    if(self.tower_data[0]["StatusPrim"] == 1):
+      self.label_status1.text = "On"
+    else:
+      self.label_status1.text = "Off"
+    if(self.tower_data[0]["StatusSec"] == 1):
+      self.label_status2.text = "On"
+    else:
+      self.label_status2.text = "Off"
+    self.label_pressure.text = str(self.tower_data[0]["Pressure"]) + " bar"
+    self.label_flowrate.text = str(self.tower_data[0]["FlowRate"]) + " L/s"
--- server_code/Backend.py
+++ server_code/Backend.py
@@ -6,10 +6,13 @@ from anvil.files import data_files
 import anvil.server
 import sqlite3
 
+
+database_file = "nuclear_facility1.db"
+
 @anvil.server.callable
 def query_nuclear_sites(self):
   query = "SELECT location FROM Facility;"
-  with sqlite3.connect(data_files["nuclear_facility.db"]) as conn:
+  with sqlite3.connect(data_files[database_file]) as conn:
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     result = cur.execute(query).fetchall()
@@ -18,7 +21,7 @@ def query_nuclear_sites(self):
 @anvil.server.callable
 def query_reactor_data(reactorID:int):
   query = f"SELECT Pressure, Temperature, NeutronFlux, ControlRodPos FROM Reactor WHERE ReactorID={reactorID};"
-  with sqlite3.connect(data_files["nuclear_facility.db"]) as conn:
+  with sqlite3.connect(data_files[database_file]) as conn:
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     result = cur.execute(query).fetchall()
@@ -27,7 +30,7 @@ def query_reactor_data(reactorID:int):
 @anvil.server.callable
 def query_reactors(facilityID: int):
   query = f"SELECT ReactorID FROM Reactor WHERE FacilityID={facilityID};"
-  with sqlite3.connect(data_files["nuclear_facility.db"]) as conn:
+  with sqlite3.connect(data_files[database_file]) as conn:
     cur = conn.cursor()
     result = cur.execute(query).fetchall()
   return result
@@ -35,7 +38,7 @@ def query_reactors(facilityID: int):
 @anvil.server.callable
 def query_facility_id(facilityName: str):
   query = f"SELECT FacilityID FROM Facility WHERE location='{facilityName}';"
-  with sqlite3.connect(data_files["nuclear_facility.db"]) as conn:
+  with sqlite3.connect(data_files[database_file]) as conn:
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     result = cur.execute(query).fetchall()
@@ -45,7 +48,7 @@ def query_facility_id(facilityName: str):
 @anvil.server.callable
 def query_overseer(facility_id : int):
   query = f"SELECT * FROM Overseer WHERE FacilityID={facility_id};"
-  with sqlite3.connect(data_files["nuclear_facility.db"]) as conn:
+  with sqlite3.connect(data_files[database_file]) as conn:
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     result = cur.execute(query).fetchall()
@@ -55,7 +58,7 @@ def query_overseer(facility_id : int):
 @anvil.server.callable
 def query_site_name(site_id: int):
   query = f"SELECT location FROM Facility WHERE FacilityID={site_id};"
-  with sqlite3.connect(data_files["nuclear_facility.db"]) as conn:
+  with sqlite3.connect(data_files[database_file]) as conn:
     cur = conn.cursor()
     result = cur.execute(query).fetchall()
   return result
@@ -63,7 +66,7 @@ def query_site_name(site_id: int):
 @anvil.server.callable
 def delete_overseer(overseer_id : int):
   query = f"DELETE FROM Overseer WHERE OverseerID={overseer_id};"
-  with sqlite3.connect(data_files["nuclear_facility.db"]) as conn:
+  with sqlite3.connect(data_files[database_file]) as conn:
     cur = conn.cursor()
     cur.execute(query)
 
@@ -71,14 +74,14 @@ def delete_overseer(overseer_id : int):
 @anvil.server.callable
 def insert_overseer(Firstname: str, Lastname: str, Clearance : int, site_id : int):
   query = f"INSERT INTO Overseer (Firstname, Lastname, Clearance, FacilityID) VALUES ('{Firstname}', '{Lastname}', {Clearance}, {site_id});"
-  with sqlite3.connect(data_files["nuclear_facility.db"]) as conn:
+  with sqlite3.connect(data_files[database_file]) as conn:
     cur = conn.cursor()
     cur.execute(query)
 
 @anvil.server.callable
 def query_security_systems(reactor_id : int):
   query = f"SELECT * FROM SafetySystems WHERE ReactorID={reactor_id};"
-  with sqlite3.connect(data_files["nuclear_facility.db"]) as conn:
+  with sqlite3.connect(data_files[database_file]) as conn:
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     result = cur.execute(query).fetchall()
@@ -87,6 +90,38 @@ def query_security_systems(reactor_id : int):
 @anvil.server.callable
 def update_security_status(security_system_id : int, toggle_value : int):
   query = f"UPDATE SafetySystems SET Status={toggle_value} WHERE SaftySystemsID={security_system_id};"
-  with sqlite3.connect(data_files["nuclear_facility.db"]) as conn:
+  with sqlite3.connect(data_files[database_file]) as conn:
+    cur = conn.cursor()
+    cur.execute(query)
+
+@anvil.server.callable
+def query_towers(reactor_id: int):
+  query = f"SELECT CoolingTowerID FROM CoolingTower WHERE ReactorID={reactor_id};"
+  with sqlite3.connect(data_files[database_file]) as conn:
+    cur = conn.cursor()
+    result = cur.execute(query).fetchall()
+  return result
+
+
+@anvil.server.callable
+def query_tower_data(tower_id : int):
+  query = f"SELECT StatusPrim, StatusSec, Pressure, FlowRate FROM CoolingTower WHERE CoolingTowerID={tower_id};"
+  with sqlite3.connect(data_files[database_file]) as conn:
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    result = cur.execute(query).fetchall()
+  return [dict(row) for row in result]
+
+@anvil.server.callable
+def update_prim_status(tower_id : int, toggle_value : int):
+  query = f"UPDATE CoolingTower SET StatusPrim={toggle_value} WHERE CoolingTowerID={tower_id};"
+  with sqlite3.connect(data_files[database_file]) as conn:
+    cur = conn.cursor()
+    cur.execute(query)
+
+@anvil.server.callable
+def update_sec_status(tower_id : int, toggle_value : int):
+  query = f"UPDATE CoolingTower SET StatusSec={toggle_value} WHERE CoolingTowerID={tower_id};"
+  with sqlite3.connect(data_files[database_file]) as conn:
     cur = conn.cursor()
     cur.execute(query)
